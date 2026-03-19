d91803f68cb3c7957e8511da0703d0fab403a2ef
jishithmp1 <jishithmp1@gmail.com>
2026-03-19 12:47:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const pool = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10
 });
 
 app.post("/create", create);
